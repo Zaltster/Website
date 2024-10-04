fda7f100e322286b4500a14889e68deae379637e
Zaltster <sms9386@psu.edu>
2024-10-03 20:45:09 -0400

    cleaner

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,12 +1,20 @@
+import React from 'react';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is my custom home page.</p>
-      <nav>
-        <a href="/MLprojects">ML Projects</a>
-        <a href="/SoftwareProjects">Software</a>
-      </nav>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-white">
+        <h1 className="text-7xl mb-6">
+          Hi, my name is <span className="font-bold">Sam</span>.
+        </h1>
+        <p className="text-5xl mb-3">
+          Software Engineer
+        </p>
+        <p className="text-4xl">
+          AI Enthusiast
+        </p>
+      </div>
+      {/* You can add more content or components here */}
     </main>
-  )
+  );
 }
