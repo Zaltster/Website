d01e3776ef20cc9005db4359fca569014cc5204d
Zaltster <sms9386@psu.edu>
2024-11-07 12:11:10 -0500

    testing layouts

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -3,7 +3,7 @@ import AnimatedContent from './components/page';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+    <main className="min-h-screen">
       <AnimatedContent />
     </main>
   );
